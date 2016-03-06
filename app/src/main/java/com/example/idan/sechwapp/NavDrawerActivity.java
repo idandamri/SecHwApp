7b0a45a803e1ab519331e28e5716d71760fe160c
idandamri <idandamri@gmail.com>
2016-03-06 23:47:53 +0200

    Done visual editing NAV
    Done editing the icons of each item in the Navagation Drawer visually

--- app/src/main/java/com/example/idan/sechwapp/NavDrawerActivity.java
+++ app/src/main/java/com/example/idan/sechwapp/NavDrawerActivity.java
@@ -27,6 +27,7 @@ public class NavDrawerActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -68,20 +69,23 @@ public class NavDrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.news) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.players) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.official_website) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.twitter) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.history) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.titles) {
+
+        }else if (id == R.id.future_games) {
+
+        }else if (id == R.id.facebook) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
